fd8e098b9cf52d423289287097501566f6cb46c7
Shukrulloh <93665101+Shukrulloh1@users.noreply.github.com>
2022-03-02 10:07:27 +0500

    successfully created banner part

--- pages/views.py
+++ pages/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, reverse
 from django.views.generic import TemplateView, CreateView
 from .forms import ContactModelForm
 from blog.models import BlogPostModel
+from .models import BannerModel
 
 
 class HomePageView(TemplateView):
@@ -10,6 +11,7 @@ class HomePageView(TemplateView):
     def get_context_data(self, **kwargs):
         context = super(HomePageView, self, **kwargs).get_context_data()
         context['last_posts'] = BlogPostModel.objects.all().order_by('-pk')[:3]
+        context['banners'] = BannerModel.objects.filter(is_active=True).order_by('-pk')
         return context
 
 
