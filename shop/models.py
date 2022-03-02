95435b54a3dc92c5397bcf2e0431fd773ab71a0a
Shukrulloh <93665101+Shukrulloh1@users.noreply.github.com>
2022-03-02 12:12:50 +0500

    ...

--- shop/models.py
+++ shop/models.py
@@ -1,3 +1,60 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+
+class TagModel(models.Model):
+    name = models.CharField(max_length=54, verbose_name=_('name'))
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name=_('created at'))
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = _('tag')
+        verbose_name_plural = _('tags')
+
+
+class CategoryModel(models.Model):
+    name = models.CharField(max_length=255, verbose_name=_('name'))
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name=_('created at'))
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = _('category')
+        verbose_name_plural = _('categories')
+
+
+class ProductModel(models.Model):
+    name = models.CharField(max_length=255, verbose_name=_('name'))
+    image = models.ImageField(upload_to='products', verbose_name=_('image'))
+    price = models.FloatField(verbose_name=_('price'))
+    real_price = models.FloatField(verbose_name=_('real price'), default=0)
+    discount = models.PositiveIntegerField(default=0, verbose_name=_('discount'))
+    short_description = models.TextField(verbose_name=_('short description'))
+    long_description = models.TextField(verbose_name=_('long description'))
+    category = models.ForeignKey(
+        CategoryModel,
+        on_delete=models.PROTECT,
+        related_name='products',
+        verbose_name=_('category')
+    )
+    tag = models.ManyToManyField(
+        TagModel,
+        related_name='products',
+        verbose_name=_('tag')
+    )
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name=_('created at'))
+
+    def get_price(self):
+        if self.discount == 0:
+            return self.price
+        return self.price - (self.price / 100) * self.discount
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = _('product')
+        verbose_name_plural = _('products')
